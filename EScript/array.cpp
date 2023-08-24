a3c82f19d5709fc9a6393bc46d196f299b940c8c
95EE093D <68942084+95EE093D@users.noreply.github.com>
2023-08-24 10:10:12 +0300

    Переделан Array.

--- EScript/array.cpp
+++ EScript/array.cpp
@@ -5,9 +5,12 @@
 
 namespace escript {
 
+std::map<std::u32string, Array::pFn> Array::_fn;
+
 Array::Array()
 {
     _managed = true;
+    buildFunctionsMap();
 }
 
 Array::~Array()
@@ -39,7 +42,6 @@ void Array::set(int64_t index, SymbolType t, uint64_t value)
 void Array::set(const std::u32string &index, PValue value)
 {
     auto key = to_utf8(index);
-    // TODO: извлечь значение, если это переменная простого типа
     setKeyValue(key, value);
 }
 
@@ -52,7 +54,7 @@ void Array::add(PValue value)
 
 std::u32string Array::uString() const
 {
-    // TODO: вывести все элементы
+    // вывести все элементы
     std::u32string s(U"[");
     for (size_t i = 0; i < _keys.size(); i++) {
         auto key = _keys[i];
@@ -72,76 +74,76 @@ void Array::setKeyValue(const std::string &key, const PValue &value)
     _items[key] = value;
 }
 
+void Array::buildFunctionsMap()
+{
+    if (!_fn.empty())
+        return;
+    _fn[U"get_length"] = &Array::call_get_length;
+    _fn[U"get"] = &Array::call_get;
+    _fn[U"set"] = &Array::call_set;
+    _fn[U"push"] = &Array::call_push;
+    _fn[U"add"] = &Array::call_push;
+}
+
+void Array::call_get_length(Processor *p)
+{
+    // вытащить из стека аргументы
+    // число аргументов, здесь д.б. 0, просто пропустить
+    auto args = loadArguments(p);
+    int64_t p1 = length();
+    p->pushToStack(p1); // результат
+}
+
+void Array::call_push(Processor *p)
+{
+    auto args = loadArguments(p);
+    if (args.empty()) {
+        p->pushToStack((int64_t)0);
+        return;
+    }
+    auto arg = args.top();
+    // пусть только переменные типа Integer
+    ObjectRecord *rec = (ObjectRecord*)(arg.value);
+    add(PValue(rec->type, rec->data));
+    p->pushToStack((int64_t)0); // OK
+}
+
+void Array::call_get(Processor *p)
+{
+    auto args = loadArguments(p);
+    if (args.empty()) {
+        p->pushToStack((int64_t)0); // OK
+        return;
+    }
+    auto index = args.top().getStringValue();
+    PValue v = this->get(index);
+    p->pushToStack(v.type, v.value64()); // OK
+}
+
+void Array::call_set(Processor *p)
+{
+    auto args = loadArguments(p);
+    auto argIndex = args.top(); // индекс/ключ
+    args.pop();
+    auto argValue = args.top(); // значение
+    // индекс может не существовать, как ключ он будет добавлен
+    // если пришла переменная, вынуть из неё значение и поместить его
+    // так чтобы тип Variable В массиве не хранился
+    // но это могут быть массивы, объекты, функции и т.п.
+    PValue v = PValue::getValue(argValue);
+    set(argIndex.getStringValue(), v);
+    p->pushToStack((int64_t)0); // OK
+}
+
 bool Array::call(const std::u32string &method, Processor *p)
 {
     if (BaseClass::call(method, p))
         return true;
-    if (method == U"get_length") {
-        // вытащить из стека аргументы
-        // число аргументов, здесь д.б. 0, просто пропустить
-        p->popFromStack();
-        int64_t p1 = length();
-        p->pushToStack(p1); // результат или 0
-        return true;
-    }
-    if (method == U"add" || method == U"push") {
-        p->popFromStack(); // число аргументов
-        auto arg = p->popFromStack();
-        // пусть только переменные типа Integer
-        ObjectRecord *rec = (ObjectRecord*)(arg.value);
-        add(PValue(rec->type, rec->data));
-        p->pushToStack((int64_t)0); // OK
-        return true;
-    }
-    if (method == U"get") {
-        auto argCount = p->popFromStack().value; // число аргументов == 1
-        assert(argCount == 1);
-        auto arg = p->popFromStack();
-        // пусть только переменные типа Integer и тип Integer
-        int index = -1;
-        if (arg.type == SymbolType::Integer)
-            index = arg.value;
-        else if (arg.type == SymbolType::Variable) {
-            ObjectRecord *rec = (ObjectRecord*)(arg.value);
-            index = rec->data;
-        }
-        PValue v = this->get(index);
-        p->pushToStack(v.type, v.value64()); // OK
-        return true;
-    }
-    if (method == U"set") {
-        auto argCount = p->popFromStack().value; // число аргументов == 2
-        assert(argCount == 2);
-        auto argValue = p->popFromStack(); // сначала значение
-        auto argIndex = p->popFromStack(); // теперь индекс/ключ
-        // пусть только переменные типа Integer и тип Integer
-        int index = -1;
-        if (argIndex.type == SymbolType::Integer)
-            index = argIndex.value;
-        else if (argIndex.type == SymbolType::Variable) {
-            ObjectRecord *rec = (ObjectRecord*)(argIndex.value);
-            index = rec->data;
-        }
-        // индекс может не существовать, как ключ он будет добавлен
-        if (argValue.type == SymbolType::Variable) {
-            ObjectRecord *rec = (ObjectRecord*)argValue.value;
-            switch (rec->type) {
-            case SymbolType::Boolean:
-            case SymbolType::Integer:
-            case SymbolType::Real:
-            case SymbolType::String:
-                argValue.type = rec->type;
-                argValue.value = rec->data;
-                break;
-            default:
-                throw std::domain_error("Not supported type of array item");
-            }
-        }
-        set(index, argValue.type, argValue.value);
-        p->pushToStack((int64_t)0); // OK
-        return true;
-    }
-    throw std::domain_error("Call of unknown method: array." + to_utf8(method));
+    auto ptrToMethod = _fn.find(method);
+    if (ptrToMethod == _fn.end())
+        throw std::domain_error("Call of unknown method: MyClass." + to_utf8(method));
+    (this->*ptrToMethod->second)(p);
+    return true;
 }
 
 } // namespace escript
--- EScript/array.h
+++ EScript/array.h
@@ -18,11 +18,13 @@ struct PValue;
  * При обращении к несуществующему индексу вставляются новые элементы (пустые).
  * Каждый элемент - это PValue.
  */
-class ESCRIPT_EXPORT Array : public AutomationObject
+class ESCRIPT_EXPORT Array final : public AutomationObject
 {
     std::map<std::string, PValue> _items;
     std::vector<std::string> _keys;
     using BaseClass = AutomationObject;
+    using pFn = void (Array::*)(Processor *p);
+    static std::map<std::u32string, pFn> _fn;
 public:
     /**
      * @brief Создаёт новый экземпляр класса Array
@@ -42,11 +44,16 @@ public:
     int64_t length() const;
     void add(PValue value);
     std::u32string uString() const;
-private:
-    void setKeyValue(const std::string &key, const PValue &value);
     // AutomationObject interface
 public:
     virtual bool call(const std::u32string &method, Processor *p) override;
+private:
+    void setKeyValue(const std::string &key, const PValue &value);
+    void buildFunctionsMap();
+    void call_get_length(Processor *p);
+    void call_push(Processor *p);
+    void call_get(Processor *p);
+    void call_set(Processor *p);
 };
 
 } // namespace escript
